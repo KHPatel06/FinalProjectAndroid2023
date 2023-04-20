c44ac7a2edf8d2c64a8b4305ef190a96c2389d83
KHPatel06 <kushp0106@gmail.com>
2023-04-20 13:02:13 -0400

    testing commit

---
+++ app/src/main/java/com/example/finalprojectandroid2023/SellFragment.kt
@@ -0,0 +1,28 @@
+package com.example.finalprojectandroid2023
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.finalprojectandroid2023.databinding.FragmentShopBinding
+
+
+class SellFragment : Fragment() {
+
+    private var _binding: FragmentShopBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentShopBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+
+
+        return rootView
+    }
+
+}
---
+++ app/src/main/java/com/example/finalprojectandroid2023/item.kt
@@ -0,0 +1,4 @@
+package com.example.finalprojectandroid2023
+
+class item(var moneyMultiplier: Double, var quantity: Int, ) {
+}
