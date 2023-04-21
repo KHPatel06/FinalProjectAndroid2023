1159790265e46985fa1e3d508992ede64af154d9
KHPatel06 <kushp0106@gmail.com>
2023-04-21 13:01:17 -0400

    commit - 04/21/2023

--- app/src/main/java/com/example/finalprojectandroid2023/MainFragment.kt
+++ app/src/main/java/com/example/finalprojectandroid2023/MainFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.finalprojectandroid2023.databinding.FragmentShopBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.ktx.database
+
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentShopBinding? = null
     private val binding get() = _binding!!
+    lateinit var dbRef : DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,7 @@ class MainFragment : Fragment() {
         _binding = FragmentShopBinding.inflate(inflater, container, false)
         val rootView = binding.root
 
+        dbRef = Firebase.database.reference
 
 
         return rootView
--- app/src/main/java/com/example/finalprojectandroid2023/item.kt
+++ app/src/main/java/com/example/finalprojectandroid2023/item.kt
@@ -1,4 +1,4 @@
 package com.example.finalprojectandroid2023
 
-class item(var moneyMultiplier: Double, var quantity: Int, ) {
+class item(var moneyMultiplier: Double, var quantity: Int, var itemName: String) {
 }
